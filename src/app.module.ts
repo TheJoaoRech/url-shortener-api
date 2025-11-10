e0e0aa48ace2671a79c9b0152fbaef82401da5e4
Joao Vitor De Oliveira Rech <joao.rech.ext@compasso.com.br>
2025-11-10 10:15:39 -0400

    feat: add UrlsModule to the application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UrlsModule } from './urls/urls.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
+    UrlsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
