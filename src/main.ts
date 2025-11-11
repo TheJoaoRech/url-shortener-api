688b40b72377199d73121d26e7f4cf8604badcf8
Joao Vitor De Oliveira Rech <joao.rech.ext@compasso.com.br>
2025-11-11 01:28:51 -0400

    chore: remove unused Logger import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { loggerConfig } from './config/logger.config';
